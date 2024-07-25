f10628a808f4bf0cb4ae7bb0b7ca66b490d6f3ef
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-25 16:38:01 +0000

    Changement de la langue en français

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {provide: LOCALE_ID, useValue: 'fr-FR'}
+  ]
 };
