73ae3ef0d7e8aefc30c7bd0ad2c6e05b57f84397
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-25 13:56:55 +0000

    afficher la liste des produits avec le component produit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProduitComponent } from "./produit/produit.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProduitComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Produits';
+  
 }
